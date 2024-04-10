590d3c008c4a57af4dcf405219256f7cc30c6268
rafixxx4k <rafa.szubert@gmail.com>
2024-04-10 21:57:03 +0200

    Add function that calculates accuracy, sensitivity, and specificity

---
+++ accuracy.py
@@ -0,0 +1,27 @@
+import numpy as np
+from collections import Counter
+
+
+def accuracy(image1, image2):
+    """
+    Calculates the accuracy, sensitivity, and specificity of two binary images.
+
+    Parameters:
+    image1 (ndarray): The ground truth binary image.
+    image2 (ndarray): The predicted binary image.
+
+    Returns:
+    tuple: A tuple containing the accuracy, sensitivity, and specificity values.
+    """
+    score = [[0, 0], [0, 0]]
+    # [[ture negative, false positive],
+    # [false negative, true positive]]
+    image1 = np.array(image1, dtype=bool)
+    image2 = np.array(image2, dtype=bool)
+    for i, j in zip(image1, image2):
+        score[i][j] += 1
+    accuracy = (score[0][0] + score[1][1]) / sum(sum(score, []))
+    sensitivity = score[1][1] / sum(score[1])
+    specificity = score[0][0] / sum(score[0])
+    print(f"{accuracy:.2f}\t{sensitivity:.2f}\t{specificity:.2f}")
+    return accuracy, sensitivity, specificity
